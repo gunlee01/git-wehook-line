d30f54597e25b9ee37054eac47191b85d7f6219f
Gunhee Lee <gunlee01@gmail.com>
2017-01-23 21:47:58 +0900

    test

--- routes/githook.js
+++ routes/githook.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 /* GET githook listing. */
 router.post('/', function(req, res, next) {
-  console.log("[GitHook]" + req.originalUrl + "--> header below --> ");
+  console.log("[GitHook] " + req.originalUrl");
+  console.log("[GitHook type] " + req.header('x-github-event') + " --> header below --> ");
+
   console.log(req.headers);
-  console.log("[GitHook continue]--> body below --> ");
+  console.log("[GitHook continue] --> body below --> ");
   console.log(req.body);
   res.send('ok');
 });
