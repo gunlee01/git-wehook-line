84a355e2bc29bf45930c1365e6bf5002a3a6ddf1
Gunhee Lee <gunlee01@gmail.com>
2017-01-23 21:37:21 +0900

    test

--- routes/githook.js
+++ routes/githook.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET githook listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   log.debug("[GitHook]" + req.originalUrl + "--> body below --> ");
   log.debug(req.body);
   res.send('ok');
